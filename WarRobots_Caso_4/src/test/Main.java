6932ce90b42f9cc9c563175f842f7a4364f00876
JorgeLuisRodriguezCruz <79291496+JorgeLuisRodriguezCruz@users.noreply.github.com>
2022-11-19 13:54:09 -0600

    prubas de stream

--- WarRobots_Caso_4/src/test/Main.java
+++ WarRobots_Caso_4/src/test/Main.java
@@ -1,5 +1,6 @@
 package test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import UI.UIWindow;
@@ -14,15 +15,43 @@ public class Main {
 		//PantallaArena vista = new PantallaArena("Titulo de ejemplo", controlador);
 		
 		//GsonSerialized g=new GsonSerialized();
+
+		//UIWindow ejemplo = new UIWindow();
+		//ejemplo.Simular();
+		
 		System.out.println("Di las palabras.");
 		
 		int clave_prueba = 12;
-		int bandera = -1;
+		boolean bandera = false;
+		boolean bandera_Dos = false;
 		
-		//int [] int_array = {1, 5, 55, 13, 14};
+		ArrayList<Integer> numeros = new ArrayList<Integer> ();
+		
+		numeros.add(1);
+		numeros.add(5);
+		numeros.add(14);
+		numeros.add(55);
+		numeros.add(13);
+		
+		int [] intArray = {2, 4, 6};
+		
+		clave_prueba = numeros.stream().filter(numero->numero==14).findFirst().orElse(-1);
+		
+		System.out.println("Esto ers lo que encontramoh -> "+clave_prueba);
+		bandera = numeros.stream().anyMatch(numero->numero==1);
+		
+		bandera_Dos = Arrays.stream(intArray).anyMatch(entero->entero==32);
+		
+		if(bandera)
+			System.out.println("SI");
+		else
+			System.out.println("NO");
+		
+		if(bandera_Dos)
+			System.out.println("SI_Dos");
+		else
+			System.out.println("NO_Dos");
 		//Arrays.stream(int_array).filter(x->x==clave_prueba).forEach(x->bandera=0);
 		
-		//UIWindow ejemplo = new UIWindow();
-		//ejemplo.Simular();
 	}
 }
