d9aaf1eb247cc136e1ea6aacba6952272082005e
JorgeLuisRodriguezCruz <79291496+JorgeLuisRodriguezCruz@users.noreply.github.com>
2022-11-22 00:02:49 -0600

    refresh move robot

--- WarRobots_Caso_4/src/robots/IshimuraKiller2001.java
+++ WarRobots_Caso_4/src/robots/IshimuraKiller2001.java
@@ -2,6 +2,7 @@ package robots;
 
 import java.awt.Graphics;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
 import common.robotBase.DamageLevel;
 import common.robotBase.IRobot;
@@ -21,7 +22,21 @@ public class IshimuraKiller2001 extends IRobot {
 
 	@Override
 	protected void refreshMove(MOVEMENT pMove, LocalTime pActionTime, Graphics g) {
-		// TODO Auto-generated method stub
+		LocalTime ahora = LocalTime.now();
+		int tiempo_Transcurrido = pActionTime.getSecond() - ahora.getSecond();
+		int casillas_Recorrer = tiempo_Transcurrido * this.speed;
 		
+		int [] array_X = {-1*casillas_Recorrer, casillas_Recorrer, 0, 0};
+		int [] array_Y = {0, 0, casillas_Recorrer, -1*casillas_Recorrer};
+		
+		
+		
+		/*
+		ArrayList <MOVEMENT> direccion_Movimiento = new ArrayList<MOVEMENT> ();
+		direccion_Movimiento.add(MOVEMENT.LEFT);
+		direccion_Movimiento.add(MOVEMENT.RIGHT);
+		direccion_Movimiento.add(MOVEMENT.DOWN);
+		direccion_Movimiento.add(MOVEMENT.UP);
+		*/
 	}
 }
