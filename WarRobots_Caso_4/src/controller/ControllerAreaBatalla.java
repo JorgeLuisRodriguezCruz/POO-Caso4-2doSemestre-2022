321b43227d31f04db36a46e9f281f448c1862e8a
JorgeLuisRodriguezCruz <79291496+JorgeLuisRodriguezCruz@users.noreply.github.com>
2022-11-23 22:42:38 -0600

    interfaz completa

--- WarRobots_Caso_4/src/controller/ControllerAreaBatalla.java
+++ WarRobots_Caso_4/src/controller/ControllerAreaBatalla.java
@@ -32,15 +32,7 @@ public class ControllerAreaBatalla {
 	public void mover (int pCodigo) {
 		switch (pCodigo) {
 			case 32:
-				//this.vista_Controlada.getProyectil().setBounds(this.vista_Controlada.getRobot().getX() - 10, this.vista_Controlada.getRobot().getY(), this.vista_Controlada.getProyectil().getWidth(), this.vista_Controlada.getProyectil().getHeight());
-				System.out.println("xP -> "+this.vista_Controlada.getProyectil().getX()+" - yP -> "+this.vista_Controlada.getProyectil().getY());
-				System.out.println("xR -> "+this.vista_Controlada.getRobot().getX()+" - yR -> "+this.vista_Controlada.getRobot().getY());
-				
-				this.vista_Controlada.getProyectil().setLocation(this.vista_Controlada.getRobot().getBounds().x, this.vista_Controlada.getRobot().getBounds().y);
-				System.out.println("Despues");
-				System.out.println("xP -> "+this.vista_Controlada.getProyectil().getX()+" - yP -> "+this.vista_Controlada.getProyectil().getY());
-				System.out.println("xR -> "+this.vista_Controlada.getRobot().getX()+" - yR -> "+this.vista_Controlada.getRobot().getY());
-				System.out.println("--------------");
+				this.vista_Controlada.getProyectil().setLocation(this.vista_Controlada.getRobot().getBounds().x-30, this.vista_Controlada.getRobot().getBounds().y);
 				
 				this.vista_Controlada.add(this.vista_Controlada.getRobot());
 				this.vista_Controlada.add(this.vista_Controlada.getProyectil());
@@ -51,33 +43,21 @@ public class ControllerAreaBatalla {
 				this.vista_Controlada.getRobot().setBounds(this.vista_Controlada.getRobot().getX() - 8, this.vista_Controlada.getRobot().getY(), this.vista_Controlada.getRobot().getWidth(), this.vista_Controlada.getRobot().getHeight());
 				this.vista_Controlada.add(this.vista_Controlada.getRobot());
 				this.vista_Controlada.repaint();
-				//this.controlledFrame.moveRobot(pPanel.getPosicion_X() - 8 , pPanel.getPosicion_Y());
-				//pFigura.setBounds(pFigura.getX() - 8, pFigura.getY(), 250, 20);
-				//pPanel.rotar(90);
 				break;
 			case 39: //Derecha
 				this.vista_Controlada.getRobot().setBounds(this.vista_Controlada.getRobot().getX() + 8, this.vista_Controlada.getRobot().getY(), this.vista_Controlada.getRobot().getWidth(), this.vista_Controlada.getRobot().getHeight());
 				this.vista_Controlada.add(this.vista_Controlada.getRobot());
 				this.vista_Controlada.repaint();
-				//this.controlledFrame.moveRobot(pPanel.getPosicion_X() + 8 , pPanel.getPosicion_Y());
-				//pFigura.setBounds(pFigura.getX() + 8, pFigura.getY(), 250, 20);
-				//pPanel.rotar(180);
 				break;
 			case 38: //Arriba
 				this.vista_Controlada.getRobot().setBounds(this.vista_Controlada.getRobot().getX(), this.vista_Controlada.getRobot().getY() - 8, this.vista_Controlada.getRobot().getWidth(), this.vista_Controlada.getRobot().getHeight());
 				this.vista_Controlada.add(this.vista_Controlada.getRobot());
 				this.vista_Controlada.repaint();
-				//this.controlledFrame.moveRobot(pPanel.getPosicion_X(), pPanel.getPosicion_Y() - 8);
-				//pFigura.setBounds(pFigura.getX(), pFigura.getY() - 8, 250, 20);
-				//pPanel.rotar(90);
 				break;
 			case 40: //Abajo
 				this.vista_Controlada.getRobot().setBounds(this.vista_Controlada.getRobot().getX(), this.vista_Controlada.getRobot().getY() + 8, this.vista_Controlada.getRobot().getWidth(), this.vista_Controlada.getRobot().getHeight());
 				this.vista_Controlada.add(this.vista_Controlada.getRobot());
 				this.vista_Controlada.repaint();
-				//this.controlledFrame.moveRobot(pPanel.getPosicion_X(), pPanel.getPosicion_Y() + 8);
-				//pFigura.setBounds(pFigura.getX(), pFigura.getY() + 8, 250, 20);
-				//pPanel.rotar(270);
 				break;
 		}
 	}
